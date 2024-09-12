c8e1f1d2ec29477e040bbd43a3a963dfd4922495
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2024-09-12 23:30:26 +0200

    add first script

---
+++ scripts/1_config_start_raffle.ts
@@ -0,0 +1,24 @@
+import hre, {ethers} from "hardhat";
+
+async function main() {
+
+  const contractAddress = "0x177b0b863b80Add7cC9824e9232a9a2dcbc7986a";
+  console.log("Contract's address: %", contractAddress);
+
+  const [owner] = await ethers.getSigners();
+  console.log("owner: %", owner);
+
+  const lottoInstance = await ethers.getContractAt("LottoClient", contractAddress);
+
+  console.log("lottoInstance: %", lottoInstance);
+  await lottoInstance.connect(owner).setConfig(4, 1, 50);
+  await lottoInstance.connect(owner).startRaffle();
+  //await lottoInstance.connect(owner).registerAttestor(attestor.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+
