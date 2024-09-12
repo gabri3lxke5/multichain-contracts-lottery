03c33931937d0376fc6d4bf05282b72e50e4a514
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2024-09-12 23:30:46 +0200

    add first script

--- scripts/1_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-const LottoClient = artifacts.require("LottoClient");
-
-module.exports = function(deployer, network, accounts) {
-  const addressOne = accounts[0];
-  deployer.deploy(LottoClient, addressOne);
-};
