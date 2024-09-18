74dc2b69205b8298ddb8b3bb465d36db169ea8d0
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2024-09-18 21:37:48 +0200

    add script to complete teh raffle

---
+++ scripts/3_complete_raffle.ts
@@ -0,0 +1,21 @@
+import hre, {ethers} from "hardhat";
+
+async function main() {
+
+  const contractAddress = "0x177b0b863b80Add7cC9824e9232a9a2dcbc7986a";
+  console.log("Contract's address: %", contractAddress);
+
+  const [owner] = await ethers.getSigners();
+  const lottoInstance = await ethers.getContractAt("LottoClient", contractAddress);
+
+  await lottoInstance.connect(owner).completeRaffle();
+  console.log("Raffle Completed");
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+
