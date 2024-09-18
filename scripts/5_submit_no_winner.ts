3f91ff41a369fe1afedfa17144eb9d469589125e
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2024-09-18 21:38:17 +0200

    add script to submit the winners

---
+++ scripts/5_submit_no_winner.ts
@@ -0,0 +1,34 @@
+import hre, {ethers} from "hardhat";
+import {expect} from "chai";
+
+// result type
+const DRAW_NUMBERS = 0;
+const CHECK_WINNERS = 1;
+
+const abiCoder = ethers.AbiCoder.defaultAbiCoder();
+
+async function main() {
+
+  const contractAddress = "0x177b0b863b80Add7cC9824e9232a9a2dcbc7986a";
+  console.log("Contract's address: %", contractAddress);
+
+  const [owner] = await ethers.getSigners();
+  const lottoInstance = await ethers.getContractAt("LottoClient", contractAddress);
+
+  const raffleId = await lottoInstance.currentRaffleId();
+  const request = abiCoder.encode(['uint[]'], [[33, 47, 5, 6]]);
+  const response = abiCoder.encode(['address[]'], [[]]);
+  const action = abiCoder.encode(['uint', 'uint8', 'bytes', 'bytes'], [raffleId, CHECK_WINNERS, request, response]);
+  const reply = '0x00' + action.substring(2);
+  await lottoInstance.connect(owner).rollupU256CondEq([], [], [], [], [reply]);
+
+  console.log("No winner sent");
+
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+
