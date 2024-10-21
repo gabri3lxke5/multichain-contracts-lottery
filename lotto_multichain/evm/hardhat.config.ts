74f919fc7da22918c04efb545ffedf23a2ed2df6
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2024-10-21 15:36:29 +0200

    use different accounts

--- lotto_multichain/evm/hardhat.config.ts
+++ lotto_multichain/evm/hardhat.config.ts
@@ -1,9 +1,12 @@
 import { HardhatUserConfig, vars, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const LOTTO_MINATO_PRIVATE_KEY = vars.get("LOTTO_MINATO_PRIVATE_KEY");
+const MINATO_OWNER_PRIVATE_KEY = vars.get("LOTTO_MINATO_OWNER_KEY");
+const MINATO_EVM_ATTESTOR_PK = vars.get("LOTTO_MINATO_ATTESTOR_KEY");
+const MINATO_USER1_PRIVATE_KEY = vars.get("LOTTO_MINATO_USER1_KEY");
+const MINATO_USER2_PRIVATE_KEY = vars.get("LOTTO_MINATO_USER2_KEY");
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -17,13 +20,25 @@ task("balance", "Prints an account's balance", async (taskArgs, hre) => {
   console.log(hre.ethers.formatEther(balance), "ETH");
 }).addParam("account", "The account's address");
 
+task("balances", "Prints the balance for all accounts", async (_taskArgs, hre) => {
+
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    const balance =  await hre.ethers.provider.getBalance(account.address);
+    const formattedBalance = hre.ethers.formatEther(balance);
+    console.log("%s - %s ETH", account.address, formattedBalance);
+  }
+
+});
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24", // replace if necessary
   networks: {
     'minato': {
       url: 'https://rpc.minato.soneium.org',
-      accounts: [LOTTO_MINATO_PRIVATE_KEY]
+      accounts: [MINATO_OWNER_PRIVATE_KEY, MINATO_EVM_ATTESTOR_PK, MINATO_USER1_PRIVATE_KEY, MINATO_USER2_PRIVATE_KEY]
     },
   },
   etherscan: {
