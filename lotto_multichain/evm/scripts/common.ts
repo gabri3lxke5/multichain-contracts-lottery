8b46842569eb3db61f6f28001eec6d82c57f718b
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2024-11-11 12:34:46 +0100

    add script to grant attestor

--- lotto_multichain/evm/scripts/common.ts
+++ lotto_multichain/evm/scripts/common.ts
@@ -11,8 +11,11 @@ export function hex(str: string): string {
   return ethers.hexlify(ethers.toUtf8Bytes(str));
 }
 
-export const contractAddress = "0xF86C68498ea16364B88B84631293CE074CeaE64f";
+export const contractAddress = "0x29621E6F2b7DBf256Ff0028dc04986C5E14Db50c";
 export const registrationContractId = 31;
 export const drawNumber = 1;
+
+export const phatAttestorAddress = "0x01e38f9e010ea0ad5808531f2722e2985f79a7c3";
+
 
 
